acad49b8601f75cfe226628cad4c6cffc9ee561d
Stephen Hill <stephen.hill@rosetta.com>
2015-01-29 14:43:45 -0800

    Linked Controllers to Views

--- src/extension/js/views/RootView.js
+++ src/extension/js/views/RootView.js
@@ -7,7 +7,16 @@
 
 var Marionette = require('backbone-shim').Marionette;
 var RootTemplate = require('templates/RootTemplate.html');
-var StepView = require('views/StepView');
+
+var Step1BaseView = require('views/Step1BaseView');
+var Step2BaseView = require('views/Step2BaseView');
+var Step3BaseView = require('views/Step3BaseView');
+var Step4BaseView = require('views/Step4BaseView');
+
+var Step1Controller = require('controllers/Step1Controller');
+var Step2Controller = require('controllers/Step2Controller');
+var Step3Controller = require('controllers/Step3Controller');
+var Step4Controller = require('controllers/Step4Controller');
 //Some childviews - item, collection, or layout - here
 
 module.exports = Marionette.LayoutView.extend({
@@ -26,8 +35,44 @@ module.exports = Marionette.LayoutView.extend({
 
   showStep: function (step) {
     this.getRegion('step' + desktopCaptureApp.currentStep).empty();
-    this.getRegion('step' + step).show(
-      new StepView()
-    );
+
+    switch(step) {
+      case 1:
+        var s1v = new Step1BaseView();
+        this.getRegion('step' + step).show(
+          s1v
+          );
+        new Step1Controller({
+          view: s1v
+        });
+      break;
+      case 2:
+        var s2v = new Step2BaseView();
+        this.getRegion('step' + step).show(
+          s2v
+          );
+        new Step2Controller({
+          view: s2v
+        });
+      break;
+      case 3:
+        var s3v = new Step3BaseView();
+        this.getRegion('step' + step).show(
+          s3v
+          );
+        new Step3Controller({
+          view: s3v
+        });
+      break;
+      case 4:
+        var s4v = new Step4BaseView();
+        this.getRegion('step' + step).show(
+          s4v
+          );
+        new Step4Controller({
+          view: s4v
+        });
+      break;
+    }
   }
 });
