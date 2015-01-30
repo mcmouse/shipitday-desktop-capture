612dd74750190fbecb10d4474a35b9eddb928a65
Tom Lagier <tom@rosetta.com>
2015-01-30 01:36:59 -0800

    Fixed requirement issue

--- src/extension/js/views/ShapeCollectionView.js
+++ src/extension/js/views/ShapeCollectionView.js
@@ -4,7 +4,8 @@
 
 /** Base view for step 2 */
 
-var Marionette = require('backbone-shim').Marionette;
+var Marionette = require('backbone-shim').Marionette,
+  TimelineView = require('views/TimelineView');
 
 module.exports = Marionette.CollectionView.extend({
   childView: TimelineView,
