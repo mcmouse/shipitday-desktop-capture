2c9077edb80367c880e335130aa88977ae5173de
Stephen Hill <stephen.hill@rosetta.com>
2015-01-29 16:04:21 -0800

    Added video to play

--- src/extension/js/controllers/Step1Controller.js
+++ src/extension/js/controllers/Step1Controller.js
@@ -17,8 +17,55 @@ module.exports = Marionette.Object.extend({
 
   setupEvents: function () {
     this.listenTo(this.view, 'record', this.recordVideo);
+    this.listenTo(this.view, 'stop', this.stopVideo);
   },
 
   recordVideo: function () {
+    chrome.desktopCapture.chooseDesktopMedia(
+      ["screen", "window"], this.onAccessApproved.bind(this));
   },
+
+  stopVideo: function() {
+    this.stream.stop();
+  },
+
+  onAccessApproved: function(id) {
+    if(!id) {
+      console.log("Access denied.");
+    }
+
+    navigator.webkitGetUserMedia({
+      audio: false,
+      video: {
+        mandatory: {
+        chromeMediaSource: "desktop",
+        chromeMediaSourceId: id
+        }
+      }
+    }, this.gotStream.bind(this), this.getUserMediaError.bind(this));
+  },
+
+  gotStream: function(stream) {
+    this.stream = stream;
+    this.view.setVideoSrc(URL.createObjectURL(stream));
+    stream.onended = this.streamEnded.bind(this);
+
+    this.recorder = RecordRTC(stream, {
+      type: "video"
+    });
+    desktopCaptureApp.models.Video.set('recorder', this.recorder);
+    this.recorder.startRecording();
+  },
+
+  getUserMediaError: function() {
+    console.log("User media error.")
+  },
+
+  streamEnded: function() {
+    this.recorder.stopRecording();
+    this.recorder.getDataURL(function(url){
+      this.view.toggleControls();
+      this.view.setVideoSrc(url);
+    }.bind(this));
+  }
 });
--- src/extension/js/views/Step1BaseView.js
+++ src/extension/js/views/Step1BaseView.js
@@ -12,14 +12,30 @@ module.exports = Marionette.ItemView.extend({
 
   ui: {
     record: '.record',
+    previewVideo: '#previewvideo',
+    stop: '.stop'
   },
 
   events: {
     'click @ui.record': 'recordVideo',
+    'click @ui.stop': 'stopVideo',
   },
 
   recordVideo: function () {
-  	console.log("First");
     this.trigger('record');
   },
+
+  stopVideo: function() {
+  	this.trigger('stop');
+  },
+
+  setVideoSrc: function(src) {
+  	var video =this.ui.previewVideo[0];
+  	video.src = src;
+  	video.play();
+  },
+
+  toggleControls:function(){
+  	this.ui.previewVideo[0].controls = true;
+  }
 });
