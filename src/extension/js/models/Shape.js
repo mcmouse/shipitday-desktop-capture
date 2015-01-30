e2588c7c1431965b379e1e10399acd1afd513596
Stephen Hill <stephen.hill@rosetta.com>
2015-01-30 08:47:38 -0800

    animate shapes

--- src/extension/js/models/Shape.js
+++ src/extension/js/models/Shape.js
@@ -17,11 +17,20 @@ module.exports = Backbone.Model.extend({
   },
 
   showShape: function () {
-
+          this.get('Shape').animate('opacity', 1,{
+        onChange: s3controller.canvas.renderAll.bind(s3controller.canvas),
+        duration: 250,
+    });
   },
 
   hideShape: function () {
-
+          this.get('Shape').animate('opacity', 0,{
+        onChange: s3controller.canvas.renderAll.bind(s3controller.canvas),
+        duration: 250,
+        onComplete: function() {
+          s3controller.canvas.remove(this.get('Shape'));
+        }.bind(this)
+    });
   },
 
   initializeShape: function (options) {
@@ -39,7 +48,8 @@ module.exports = Backbone.Model.extend({
       var shape = new fabric.Rect({
         fill: options.color,
         width: 100,
-        height: 100
+        height: 100,
+        opacity: 0
       });
 
       this.trigger('shapeLoaded', shape);
@@ -51,7 +61,8 @@ module.exports = Backbone.Model.extend({
         width: 125,
         height: 125,
         stroke: 'red',
-        strokeWidth: 5
+        strokeWidth: 5,
+        opacity: 0
       });
 
       this.trigger('shapeLoaded', shape);
@@ -60,7 +71,8 @@ module.exports = Backbone.Model.extend({
     case 'text':
       var shape = new fabric.IText("Hello world!", {
         fill: options.color,
-        fontSize: 20
+        fontSize: 20,
+        opacity: 0
       });
 
       this.trigger('shapeLoaded', shape);
@@ -71,7 +83,8 @@ module.exports = Backbone.Model.extend({
         var group = new fabric.PathGroup(objects, {
           width: 30,
           height: 40,
-          fill: 'red'
+          fill: 'red',
+        opacity: 0
         });
         this.trigger('shapeLoaded', group);
       }.bind(this));
@@ -81,7 +94,8 @@ module.exports = Backbone.Model.extend({
       var shape = new fabric.Triangle({
         fill: options.color,
         width: 100,
-        height: 100
+        height: 100,
+        opacity: 0
       });
 
       this.trigger('shapeLoaded', shape);
