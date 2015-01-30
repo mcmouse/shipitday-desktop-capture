f40843065e81fda1d8102cbd9a9f334ad5c8cdd7
Kevin Knopp <kevin.knopp@rosetta.com>
2015-01-30 08:23:03 -0800

    updated slider ui

--- src/extension/js/views/TimelineView.js
+++ src/extension/js/views/TimelineView.js
@@ -14,7 +14,7 @@ module.exports = Marionette.ItemView.extend({
   },
   onShow: function() {
   	this.model.initializeRangeBar();
-  	this.$ButtonClick = $('<button>X</button>')
+  	this.$ButtonClick = $("<button class='close-timeline btn-floating btn waves-effect waves-light red'><i class='mdi-navigation-close'></i></button>")
   	this.$el.append(this.$ButtonClick.get(0));
   	this.$ButtonClick.click(function(){
   		this.$ButtonClick.parent().remove();
